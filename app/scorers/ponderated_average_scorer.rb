e897f701ca28fa54149b36864d8c4fa7048cc18f
Jorge Smulevici <Jsmulevici@Jsmulevici-MBK13.local>
2019-01-23 16:46:32 +0200

    fixes on PonderatedAverageScorer

--- app/scorers/ponderated_average_scorer.rb
+++ app/scorers/ponderated_average_scorer.rb
@@ -1,7 +1,7 @@
 class PonderatedAverageScorer < SlotAttendeeScorer
-  def initialize(slot, attendee, features, features_weight, attendee_constraints = [])
+  def initialize(slot, attendee, features, features_weights, attendee_constraints = [])
     @features = features
-    @features_weight = features_weight
+    @features_weights = features_weights
     @attendee_constraints = attendee_constraints
     super(slot, attendee)
   end
@@ -12,13 +12,13 @@ class PonderatedAverageScorer < SlotAttendeeScorer
     return 0 if @features.size.zero?
 
     # Attendee constrains
-    attendee_constraints.each do |constraint|
+    @attendee_constraints.each do |constraint|
       return 0 if constraint.new(@slot, @attendee).enabled?
     end
-    
+
     # Attendee features
-    features_result = @features.map { |feature| feature.new(@slot, @attendee).enabled? }
-    weights = features_weight.each_with_index { |specific_weight, index| features_result[index] ? features_result[index] * specific_weight : 0 }
-    weights.sum /  @features.size
-  end
+    features_result = @features.map { |feature| feature.new(@slot, @attendee).enabled? ? 1 : 0 } [1,0,1,0,1,0,1]
+    weights = @features_weights.each_with_index { |specific_weight, index| features_result[index] * specific_weight }
+    1 - (weights.sum / @features.size)
+  end 
 end
